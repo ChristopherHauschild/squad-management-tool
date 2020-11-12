1a0e0bf160b31273e1f2016a1df15466ffccaeda
ChristopherHauschild <chrishaus01@gmail.com>
2020-11-12 19:33:48 -0300

    refactor: add header to card component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container } from './styled';
 
-const Button = ({ children }) => {
-  return <Container>{children}</Container>;
+const Button = ({ onClick, children }) => {
+  return <Container onClick={onClick}>{children}</Container>;
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import media from 'styled-media-query';
 
 import { Card } from 'antd';
 
@@ -7,4 +8,37 @@ export const Container = styled(Card)`
   box-shadow: rgba(0, 0, 0, 0.04) 0px 0.4px 2px;
   display: flex;
   flex-direction: column;
+
+  .ant-card-body {
+    padding: ${({ theme }) => theme.spacings.small} 0px;
+  }
+`;
+
+export const Header = styled.header`
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.border.color};
+  display: flex;
+  justify-content: space-between;
+  padding: 0px ${({ theme }) => theme.spacings.small};
+  padding-bottom: ${({ theme }) => theme.spacings.xsmall};
+  margin-bottom: ${({ theme }) => theme.spacings.small};
+
+  h1 {
+    color: ${({ theme }) => theme.colors.secondary};
+    font-size: ${({ theme }) => theme.font.sizes.xxlarge};
+    font-weight: ${({ theme }) => theme.font.bold};
+  }
+
+  ${media.lessThan('576px')`
+    h1 {
+      font-size: ${({ theme }) => theme.font.sizes.xlarge};
+    }
+  `}
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  padding: 0px ${({ theme }) => theme.spacings.small};
 `;
