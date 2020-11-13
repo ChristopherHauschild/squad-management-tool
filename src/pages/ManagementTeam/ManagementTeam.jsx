23073065f80c71f28c79d8c3a00a312109f8d177
ChristopherHauschild <chrishaus01@gmail.com>
2020-11-13 20:51:50 -0300

    feat: create management-team page

---
+++ src/pages/ManagementTeam/ManagementTeam.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from 'components/Layout';
+
+import ManagementTeamPage from './presentation/ManagementTeamPage';
+
+const ManagementTeam = () => {
+  return (
+    <Layout>
+      <ManagementTeamPage />
+    </Layout>
+  );
+};
+
+export default ManagementTeam;
---
+++ src/pages/ManagementTeam/presentation/ManagementTeamPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Col, Row } from 'antd';
+
+const ManagementTeamPage = () => {
+  return (
+    <Row>
+      <Col span={24} />
+    </Row>
+  );
+};
+
+export default ManagementTeamPage;
